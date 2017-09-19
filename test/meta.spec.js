5d966a8c77efd7ba623fc32fd9cf2ca7195cd5be
Shawn Allen <shawn.allen@github.com>
2017-09-19 10:41:39 -0700

    npm run prettier :sparkles:

--- test/meta.spec.js
+++ test/meta.spec.js
@@ -5,33 +5,30 @@ const vm = require('vm')
 
 const tests = ['../README.md', '../api.md', 'meta.spec.md']
   .reduce((tests, file) => {
-    return tests.concat(
-      blocks.fromFileSync(path.join(__dirname, file))
-    )
+    return tests.concat(blocks.fromFileSync(path.join(__dirname, file)))
   }, [])
-  .filter(block => (
-    block.lang === 'js' &&
-    block.info.skip !== 'true' &&
-    block.value.indexOf('test') > -1
-  ))
+  .filter(
+    block =>
+      block.lang === 'js' &&
+      block.info.skip !== 'true' &&
+      block.value.indexOf('test') > -1
+  )
   .map(block => {
     test(block.title, t => {
       const script = new vm.Script(block.value, {
         filename: block.source.file,
         lineOffset: block.source.line,
-        displayErrors: true,
+        displayErrors: true
       })
       const context = vm.createContext({
         __dirname,
         block,
         blocks,
-        test: t,
+        test: t
       })
       const result = script.runInContext(context)
       if (result instanceof Promise) {
-        return result
-          .then(msg => t.pass(msg))
-          .catch(error => t.fail(error))
+        return result.then(msg => t.pass(msg)).catch(error => t.fail(error))
       } else {
         return t.pass(result)
       }
